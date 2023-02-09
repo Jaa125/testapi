081e88588646ea4143350acae07aa7d6068c719f
Jaa125 <75535272+Jaa125@users.noreply.github.com>
2023-02-09 22:53:50 +0100

    first commit1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const axios = require('axios');
 const app = express()
+const cors = require('cors')
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 const authRout = require('./routes/auth')
@@ -11,7 +12,14 @@ const regionRout = require('./routes/regions')
 
 dotenv.config()
 app.use(express.json())
-
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    // res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,text');
+    next();
+});
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
